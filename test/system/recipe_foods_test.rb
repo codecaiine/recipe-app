bb0a38bb31319c5b53937913abbeff220d3a4e0e
codecaiine <akabrouyannickn@gmail.com>
2022-01-11 13:28:13 +0000

    Create food recipe model and table

---
+++ test/system/recipe_foods_test.rb
@@ -0,0 +1,47 @@
+require "application_system_test_case"
+
+class RecipeFoodsTest < ApplicationSystemTestCase
+  setup do
+    @recipe_food = recipe_foods(:one)
+  end
+
+  test "visiting the index" do
+    visit recipe_foods_url
+    assert_selector "h1", text: "Recipe Foods"
+  end
+
+  test "creating a Recipe food" do
+    visit recipe_foods_url
+    click_on "New Recipe Food"
+
+    fill_in "Food", with: @recipe_food.food_id
+    fill_in "Quantity", with: @recipe_food.quantity
+    fill_in "Recipe", with: @recipe_food.recipe_id
+    click_on "Create Recipe food"
+
+    assert_text "Recipe food was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a Recipe food" do
+    visit recipe_foods_url
+    click_on "Edit", match: :first
+
+    fill_in "Food", with: @recipe_food.food_id
+    fill_in "Quantity", with: @recipe_food.quantity
+    fill_in "Recipe", with: @recipe_food.recipe_id
+    click_on "Update Recipe food"
+
+    assert_text "Recipe food was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a Recipe food" do
+    visit recipe_foods_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "Recipe food was successfully destroyed"
+  end
+end
