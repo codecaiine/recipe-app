787260b926401b493112cd93a6c7deee14d6966f
codecaiine <akabrouyannickn@gmail.com>
2022-01-12 23:56:37 +0000

    Remove controllers and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'recipe/new'
-  get 'recipe/edit'
-  get 'recipe/show'
-  get 'recipe/index'
-  get 'food/new'
-  get 'food/edit'
-  get 'food/show'
-  get 'food/index'
   devise_for :users
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
