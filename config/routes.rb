df2e7dcdf8b3b39470b9ee1f2d3060030859dbae
codecaiine <akabrouyannickn@gmail.com>
2022-01-12 23:58:19 +0000

    Generate correct user controller and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/index'
   devise_for :users
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
