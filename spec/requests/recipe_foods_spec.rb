5cd0e737f4c7cdd4ae56316f3d42c1bb65281c6c
codecaiine <akabrouyannickn@gmail.com>
2022-01-14 10:09:51 +0000

    Fix linters errors

--- spec/requests/recipe_foods_spec.rb
+++ spec/requests/recipe_foods_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "RecipeFoods", type: :request do
-  describe "GET /new" do
-    it "returns http success" do
-      get "/recipe_foods/new"
+RSpec.describe 'RecipeFoods', type: :request do
+  describe 'GET /new' do
+    it 'returns http success' do
+      get '/recipe_foods/new'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
